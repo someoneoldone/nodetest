3e701078fc85ebb500ac3f06dffc5e5baf9243cf
someoneoldone <140854601+someoneoldone@users.noreply.github.com>
2023-07-29 02:42:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mysql = require('mysql')
 const cors= require('cors')
-
+console.log("=============================")
+console.log("OhYEahhhhhhhhhhh")
+console.log("=============================")
 const app=express()
 app.use(cors())
 app.use(express.json())
